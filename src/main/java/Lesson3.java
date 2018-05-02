0d13b8525d8696fe38872929edb6cc401a53caaf
Дмитрий <Thedimok@rambler.ru>
2018-05-03 01:45:23 +0300

    Сортировка с 1000000 Значений проходит несколько часов(((

---
+++ src/main/java/Lesson3.java
@@ -0,0 +1,206 @@
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lesson3 {
+
+    private static long countSortSheikUl = 0;
+    private static long countSortUl = 0;
+    private static long countSort = 0;
+    private static double timeSortUl;
+    private static double timeSort;
+    private static double timeSortSheikUl;
+
+    public static void main(String[] args) {
+//        int size = 10000;
+
+        double j = 1;
+        List<int[]> dadArray = new ArrayList<int[]>();
+        while (j < 4) {
+
+            int[] arr = new int[(int) (Math.pow(100, j))];
+
+            int i = 0;
+            while (i < arr.length) {
+                arr[i] = (int) (1 + Math.random() * 99);
+//            System.out.print(arr[i] + " ");
+                i++;
+            }
+
+            int[] arr2 = (int[]) arr.clone();
+            int[] arr3 = (int[]) arr.clone();
+
+            dadArray.add(arr);
+            dadArray.add(arr2);
+            dadArray.add(arr3);
+            j++;
+        }
+        System.out.println("create arr");
+
+        System.out.println();
+        System.out.println("start sort " + dadArray.get(0).length + ": " + LocalTime.now());
+        sort(dadArray.get(2));
+        sortUl(dadArray.get(1));
+        sortSheikUl(dadArray.get(0));
+        System.out.println("Сортировка пузырьком:            " + countSort + " циклов перезаписи и " + timeSort + " секунд выполнение");
+        System.out.println("Сортировка пузырьком улучшенная: " + countSortUl + " циклов перезаписи и " + timeSortUl + " секунд выполнение");
+        System.out.println("Сортировка шейкером:             " + countSortSheikUl + " циклов перезаписи и " + timeSortSheikUl + " секунд выполнение");
+        System.out.println("start sort " + dadArray.get(3).length + ": " + LocalTime.now());
+        sort(dadArray.get(5));
+        sortUl(dadArray.get(4));
+        sortSheikUl(dadArray.get(3));
+        System.out.println("Сортировка пузырьком:            " + countSort + " циклов перезаписи и " + timeSort + " секунд выполнение");
+        System.out.println("Сортировка пузырьком улучшенная: " + countSortUl + " циклов перезаписи и " + timeSortUl + " секунд выполнение");
+        System.out.println("Сортировка шейкером:             " + countSortSheikUl + " циклов перезаписи и " + timeSortSheikUl + " секунд выполнение");
+        System.out.println("start sort " + dadArray.get(6).length + ": " + LocalTime.now());
+        sort(dadArray.get(8));
+        sortUl(dadArray.get(7));
+        sortSheikUl(dadArray.get(6));
+        System.out.println("Сортировка пузырьком:            " + countSort + " циклов перезаписи и " + timeSort + " секунд выполнение");
+        System.out.println("Сортировка пузырьком улучшенная: " + countSortUl + " циклов перезаписи и " + timeSortUl + " секунд выполнение");
+        System.out.println("Сортировка шейкером:             " + countSortSheikUl + " циклов перезаписи и " + timeSortSheikUl + " секунд выполнение");
+
+
+    }
+
+    private static void sort(int[] array) {
+        double startSort = System.currentTimeMillis();
+        int i = 0;
+        int max;
+        int j = 0;
+
+        while (j < array.length - 1) {
+            while (i + 1 < array.length) {
+
+
+                if (i + 1 < array.length) {
+
+                    if (array[i] > array[i + 1]) {
+                        max = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = max;
+                        countSort++;
+                    }
+                    i++;
+                }
+            }
+            j++;
+            i = 0;
+        }
+        double endSort = System.currentTimeMillis();
+        timeSort = (endSort - startSort) / 1000;
+    }
+
+    private static void sortUl(int[] array) {
+        double startSortUl = System.currentTimeMillis();
+        int i = 0;
+        int j = 0;
+        int max;
+        int fix = 0;
+
+        while (j < array.length - fix) {
+            while (i + 1 < array.length - fix) {
+
+
+                if (i + 1 < array.length) {
+
+                    if (array[i] > array[i + 1]) {
+                        max = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = max;
+                        countSortUl++;
+                    }
+                    i++;
+                }
+
+            }
+            fix++;
+            i = 0;
+
+            j++;
+        }
+        double endSortUl = System.currentTimeMillis();
+        timeSortUl = (endSortUl - startSortUl) / 1000;
+    }
+
+    private static void printArray(int[] arr) {
+
+        int i = 0;
+        while (i < arr.length) {
+            System.out.print(arr[i] + " ");
+            i++;
+        }
+        System.out.println();
+        System.out.println("Count: " + countSortSheikUl);
+    }
+
+    private static void sortSheikUl(int[] array) {
+        double startSortSheikUl = System.currentTimeMillis();
+        int i = 0;
+        int j = 0;
+        int max;
+        int min;
+        int fix = 0;
+        int fix2 = 0;
+
+        while (j < array.length - 1) {
+            if (j % 2 == 0) {
+                while (i + 1 + fix2 < array.length - fix) {
+
+
+                    if (i + 1 < array.length) {
+
+                        if (array[i + fix2] > array[i + 1]) {
+                            max = array[i];
+                            array[i] = array[i + 1];
+                            array[i + 1] = max;
+                            countSortSheikUl++;
+                        }
+                        i++;
+                    }
+
+                }
+                fix++;
+            } else {
+                while (i > 0 + fix2) {
+
+                    if (i - fix - 1 >= 0) {
+
+                        if (array[i - fix] < array[i - fix - 1]) {
+                            min = array[i - fix];
+                            array[i - fix] = array[i - fix - 1];
+                            array[i - fix - 1] = min;
+                            countSortSheikUl++;
+                        }
+                    }
+                    i--;
+
+                }
+                fix2++;
+            }
+
+            if (fix2 == array.length / 2) {
+                j = array.length;
+            } else {
+                j++;
+            }
+        }
+        double endSortSheikUl = System.currentTimeMillis();
+        timeSortSheikUl = (endSortSheikUl - startSortSheikUl) / 1000;
+    }
+
+    private static int binarySearch(int[] arr, int search) {
+
+        int centr = arr.length / 2;
+        while () {
+            if (search == arr[centr]) return centr;
+            else if (search > arr[centr]) centr = (arr.length - centr) / 2;
+            else centr = centr / 2;
+        }
+    }
+}
---
+++ src/main/java/lesson2.java
@@ -0,0 +1,125 @@
+import java.lang.reflect.Array;
+
+/*
+        Пантелеев Дмитрий
+*/
+public class lesson2 {
+
+    private static int resultRec2 = 1;
+    private static int resultRec = 0;
+    private static int resultDegree = 1;
+    private static int resultfun = 0;
+    private static double count = 0;
+    private static int resultCount = 0;
+
+    public static void main(String[] args) {
+//        1. Реализовать функцию перевода чисел из десятичной системы в двоичную, используя
+//           рекурсию.
+        funDecInBin(123);
+
+
+//        2. Реализовать функцию возведения числа a в степень b:
+        rec2(5, 11);//чётность и рекурсия
+        rec(5, 11);//рекурсия
+        degree(5, 11);//степень
+
+//        3. Исполнитель Калькулятор преобразует целое число, записанное на экране.
+//           У исполнителя две команды, каждой команде присвоен номер:
+//           Прибавь 1
+//           2.Умножь на 2
+//           Первая команда увеличивает число на экране на 1,
+//           вторая увеличивает это число в 2 раза.
+//           Сколько существует программ, которые число 3 преобразуют в число 20?
+//           а) с использованием массива;
+//           б) с использованием рекурсии.
+
+//        calcMass();
+        calcRec(3, 20, 1, 2);
+
+
+//        int i = 1;
+//        int[] array = new int[100];
+//        while (i < 21) {
+//            array[i] = i+2;
+//            i++;
+//        }
+
+        System.out.println("Результат цикла: " + resultDegree);
+        System.out.println("Результат рекурсии: " + resultRec);
+        System.out.println("Результат рекурсии учитывая свойство чётности степени: " + resultRec2);
+        System.out.println("Результат бинаризации: " + resultfun);
+
+
+    }
+
+    //  3. Исполнитель Калькулятор преобразует целое число, записанное на экране.
+    private static int calcRec(int start, int end, int sum, int mult) {
+        if (start == end) {
+            return 1;
+        } else if (start % 2 == 0) {
+            calcRec(start += (start - sum) + (start / mult), end, sum, mult);
+        } else {
+            calcRec(start += (start - sum), end, sum, mult);
+        }
+
+
+        return end;
+    }
+
+//    private static int calcMass() {
+//
+//        Array[] calcMassArr = new Array[];
+//
+//    }
+
+    //  1. Реализовать функцию перевода чисел из десятичной системы в двоичную,
+    //  используя рекурсию.
+    private static int funDecInBin(int ten) {
+        if (ten == 0) {
+            return 1;
+        }
+        resultfun = (int) (resultfun + (ten % 2) * (Math.pow(10, count)));
+        count++;
+
+        funDecInBin(ten /= 2);
+
+        return resultfun;
+    }
+
+    //2. Реализовать функцию возведения числа a в степень b:
+    //   c. *Рекурсивно, используя свойство чётности степени
+    private static int rec2(int dec, int pow) {
+//        if (resultRec2 == 0){resultRec2 = dec;}
+        if (pow == 0) return 1;
+        if (pow % 2 != 0) {
+            resultRec2 *= dec;
+            --pow;
+        }
+        rec2(dec *= dec, pow /= 2);
+
+        return resultRec2;
+    }
+
+    //    b. Рекурсивно.
+    private static int rec(int dec, int pow) {
+        if (resultRec == 0) {
+            resultRec = dec;
+        }
+        if (pow == 1) {
+            return resultRec;
+        }
+
+        return resultRec *= rec(dec, --pow);
+
+    }
+
+    //    a. Без рекурсии.
+    private static int degree(int dec, int pow) {
+
+        while (pow > 0) {
+            resultDegree *= dec;
+            pow--;
+        }
+        return resultDegree;
+    }
+}
